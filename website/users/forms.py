d704a95f8080d33eafb76b056b8316b9ffd6a2a9
fixzoro <samwinchester@ya.md>
2024-07-13 22:42:31 +0300

    Revert "Чат?"
    This reverts commit a463ef162a8c43989fbe73a9e23681df57a028c8.

--- website/users/forms.py
+++ website/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile,Message
+from .models import Profile
 
 
 class UserOurRegistraion(UserCreationForm):
@@ -28,14 +28,3 @@ class ProfileImage(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["img"]
-
-
-class MessageForm(forms.ModelForm):
-
-    class Meta:
-        model = Message
-        fields = ["message","user","task"]
-        widgets = {'user': forms.HiddenInput(),
-                   'task': forms.HiddenInput(),
-                   'message': forms.Textarea(attrs={'placeholder': 'текст сообщения'})}
-

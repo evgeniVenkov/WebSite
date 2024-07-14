dbc5175af8256c6d59586ebb1fae1bd0b14edc6a
fixzoro <samwinchester@ya.md>
2024-07-14 15:19:59 +0300

    Delete

--- website/tasks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.TasksPage.as_view(), name="tasks"),
-    path("<slug>", views.TaskPage.as_view(), name="task"),
-]

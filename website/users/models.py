11dcd54414e842ea351765fec1255ce621ad97a0
evgeniVenkov <evgenijvenkov183@gmail.com>
2024-07-28 16:17:02 +0300

    add commit

--- website/users/models.py
+++ website/users/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from PIL import Image
 
 
 TYPE_ACCOUNT = (("full", "Полный пакет"), ("free", "Бесплатный пакет"))
 
+class CustomUser(AbstractUser):
+    friends = models.ManyToManyField('self', through='FriendRequest', symmetrical=False,related_name='friends')
+
+    def __str__(self):
+        return self.username
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
